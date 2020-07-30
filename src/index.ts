ed1b5142a81e463501afdd9018bdfa37d25f059b
progfay <shun1856shun@gmail.com>
2020-07-30 23:54:45 +0900

    feat: self impl to console

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-export const group = (groupName: string, func: () => void) => {
-  console.group(groupName)
-  func()
-  console.groupEnd()
+import _Console from 'console'
+
+interface Console {
+  readonly _console: typeof _Console,
+  _indentCount: number,
+  readonly _indentText: string,
+
+  group: (name: string, func: (console: Console) => void) => void
+  log: (...data: any[]) => void
+}
+
+export const play = (func: (console: Console) => void) => {
+  const console: Console = {
+    _console: new _Console.Console({
+      stdout: process.stdout,
+      stderr: process.stderr,
+    }),
+    _indentCount: 0,
+    _indentText: '    ',
+
+    group: (name, func) => {
+      process.stdout.write(console._indentText.repeat(console._indentCount) + name + '\n')
+      console._indentCount++
+      func(console)
+      console._indentCount--
+    },
+
+    log: (...args) => {
+      process.stdout.write(console._indentText.repeat(console._indentCount))
+      console._console.log(...args)
+    }
+  }
+
+  func(console)
 }
