c206810595219f63c134b496ec3a6f0423ee339d
progfay <shun1856shun@gmail.com>
2020-08-04 23:44:19 +0900

    test: add async unit

--- tests/index.js
+++ tests/index.js
@@ -1,22 +1,29 @@
 const { play } = require('../lib')
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 play([
   'first',
-  console => {
+  async console => {
     console.log('hoge', 'fuga')
+    await sleep(1000)
     console.log('piyo', 'buzz')
   },
   [
     'second',
     console => {
       console.log(JSON.parse(NaN))
+      console.log('piyo', 'buzz')
     },
     console => {
       console.log(`t${'o'.repeat(300)} long`)
       console.log(`t${'o'.repeat(300)} long`)
     },
-    console => {
-      console.log(new Date())
+    async console => {
+      for (let i = 0; i < 30; i++) {
+        await sleep(100)
+        console.log(new Date())
+      }
     },
     [
       'third',
