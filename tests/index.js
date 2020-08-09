bbadb283f83e93dafc5431218128077df8462897
progfay <shun1856shun@gmail.com>
2020-08-09 23:03:41 +0900

    test: support functional style

--- tests/index.js
+++ tests/index.js
@@ -2,50 +2,37 @@ const { play } = require('../lib')
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-play([
-  'first',
-  async console => {
+play((bundle, run) => {
+  run('first', async console => {
     console.log('hoge', 'fuga')
     await sleep(1000)
     console.log('piyo', 'buzz')
-  },
-  [
-    'second',
-    console => {
+  })
+
+  bundle('second', (bundle, run) => {
+    run('error handling', () => {
       console.log(JSON.parse(NaN))
       console.log('piyo', 'buzz')
-    },
-    console => {
+    })
+
+    run('long', console => {
       console.log(`t${'o'.repeat(300)} long`)
       console.log(`t${'o'.repeat(300)} long`)
-    },
-    async console => {
+    })
+
+    run('date', async console => {
       for (let i = 0; i < 30; i++) {
         await sleep(100)
         console.log(new Date())
       }
-    },
-    [
-      'third',
-      [
-        'fourth',
-        [
-          'fifth',
-          console => {
-            console.log(1234)
-          }
-        ]
-      ]
-    ]
-  ]
-])
+    })
+  })
 
-play((group, unit) => {
-  group('adfad', (group, unit) => {
-    group('hoge', (group, unit) => {
-      unit('unit', (console) => {
-        console.log(2345)
+  bundle('nest', (bundle, run) => {
+    bundle('re:nest', (bundle, run) => {
+      run('re:re:nest', console => {
+        console.log(1234)
       })
     })
   })
-})
+}, { debug: false })
