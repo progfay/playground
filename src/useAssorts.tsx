752f39f9cd40d2004010bb3fb9d199c12a7e53a6
progfay <shun1856shun@gmail.com>
2020-08-10 23:18:04 +0900

    refactor: rename uuidv4 to uuid

--- src/useAssorts.tsx
+++ src/useAssorts.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useApp } from 'ink'
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuid } from 'uuid'
 
 import type { Run, Group, Pack, Assort } from './types'
 
@@ -15,7 +15,7 @@ export const useAssorts: UseAssortsHooks = ({ nest, level }) => {
       const group: Group = (name, nest) => {
         setAssorts(states => [...states, {
           type: 'pack',
-          uuid: uuidv4(),
+          uuid: uuid(),
           level: level + 1,
           name,
           nest
@@ -25,7 +25,7 @@ export const useAssorts: UseAssortsHooks = ({ nest, level }) => {
       const run: Run = (name, func) => {
         setAssorts(states => [...states, {
           type: 'unit',
-          uuid: uuidv4(),
+          uuid: uuid(),
           level: level + 1,
           name,
           func
